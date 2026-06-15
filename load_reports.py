bbaaefdbdf5a808fc66469a8c285c3e898f24775
Eduardo R <dudub9go@hotmail.com>
2026-06-15 15:47:42 -0300

    Adicionando metodo que le a lista os relatorios do json

---
+++ load_reports.py
@@ -0,0 +1,17 @@
+import json
+
+def load_reports(path_file):
+    """Lê o arquivo JSON e retorna a lista de dicionários."""
+    try:
+        with open(path_file, 'r', encoding='utf-8') as file:
+            dados = json.load(file)
+            print(f"Sucesso! {len(dados)} relatórios carregados.")
+            return dados
+    except FileNotFoundError:
+        print("Erro: Arquivo não encontrado.")
+        return []
+# Teste de execução local
+if __name__ == "__main__":
+    reports = load_reports('dataset.json')
+    if reports:
+        print("Exemplo do primeiro relatório:", reports[0]['texto'])
